c6eb73868c131cc1f8802a8400a1965f26364c4f
Lucas M. T. Friedrich <frielucas@gmail.com>
2023-03-17 09:34:57 -0300

    alterações no form e navbar

--- enviar_email.php
+++ enviar_email.php
@@ -12,20 +12,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $destinatario = "frielucas@gmail.com";
   
   // Define o assunto do e-mail
-  $assunto = "Nova mensagem de $nome";
+  $assunto = "Mensagem vinda do site de $nome";
   
+  // Monta o corpo2 do e-mail
+  $corpo2 = "Prezado(a) Sr(a). $nome,\n\n";
+  $corpo2 .= "Gostaríamos de agradecer pelo seu contato através do nosso site e informamos que sua mensagem foi recebida com sucesso.\n";
+  $corpo2 .= "Entraremos em contato o mais breve possível para dar continuidade ao seu pedido.\n\n";
+  $corpo2 .= "Segue abaixo as informações que você nos forneceu:\n";
+
   // Monta o corpo do e-mail
-  $corpo = "Nome: $nome\n";
+  $corpo .= "Nome: $nome\n";
   $corpo .= "E-mail: $email\n";
   $corpo .= "Telefone: $telefone\n";
-  $corpo .= "Mensagem: $mensagem\n";
+  $corpo .= "Mensagem: $mensagem\n\n";
+  $corpo .= "Atenciosamente,\n";
+  $corpo .= "Equipe do Site\n";
+  $corpo2 .= $corpo;
+
+  // Cabeçalhos adicionais
+  $headers = "From: Bortolin Consultoria <$destinatario>\r\n"; // Define o remetente
   
   // Envia o e-mail
-  if (mail($destinatario, $assunto, $corpo)) {
-    echo "Sua mensagem foi enviada com sucesso!";
-  } else {
+  if (mail($destinatario, $assunto, $corpo, $headers)) {
+    $_POST = array(); // Limpa o formulário
+    echo "<script>alert('Sua mensagem foi enviada com sucesso!'); window.history.back();</script>";
+    // Envia o e-mail pro mano que mandou o form
+    mail($email, 'Confirmação de envio de e-mail.', $corpo2, $headers);
+    exit;
+  } else  {
     echo "Ocorreu um erro ao enviar a mensagem.";
   }
-  
 }
 ?>
